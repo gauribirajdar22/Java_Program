dc49b0273a5151139accebcb5b170a0f01f0a8fb
Gauri Birajdar <birajdargauri22@gmail.com>
2026-02-20 12:01:24 +0530

    Create gauri.cpp

---
+++ gauri.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// This line allows us to use standard library names like 'cout' directly
+using namespace std;
+
+int main() {
+  // The 'cout' object is used with the insertion operator '<<' to print text
+  cout << "Hello, World!"; 
+  
+  // 'return 0;' ends the main function and indicates the program ran successfully
+  return 0;
+}
